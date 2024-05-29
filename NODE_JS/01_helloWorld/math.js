d2983644f4a20177027118899b25cc4b87749192
Sivram Pradhan <programmersiv21@gmail.com>
2024-05-30 03:13:57 +0530

    changes

---
+++ NODE_JS/01_helloWorld/math.js
@@ -0,0 +1,30 @@
+// function add (a,b){
+//     return a+b;
+// }
+
+// function sub (a,b){
+//     return a-b;
+// }
+
+
+// module.exports = add;
+
+// module.exports = {
+//     add,
+//     sub,
+// }
+
+// module.exports = {
+//     addFn: add,
+//     subFn: sub,
+// }
+
+// module.exports = {
+//     add,
+//     sub,
+// }
+
+
+exports.add1 = (a,b) => a+b;
+
+exports.sub1 = (a,b) => a-b;
