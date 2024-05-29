70048bfd42c9afd1efec788dd075c8d8c7cf9220
Sivram Pradhan <programmersiv21@gmail.com>
2024-05-30 03:13:47 +0530

    changes

---
+++ NODE_JS/01_helloWorld/hello.js
@@ -0,0 +1,15 @@
+const math = require("./math")
+// const {add ,sub} = require("./math")
+
+// console.log(math.addFn(2,4))
+// console.log(math.subFn(4,2))
+
+
+// console.log(math.add(2,4))
+// console.log(math.sub(4,2))
+
+// console.log(add(2,4))
+// console.log(sub(4,2))
+
+console.log(math.add1(2,4));
+console.log(math.sub1(4,2));
